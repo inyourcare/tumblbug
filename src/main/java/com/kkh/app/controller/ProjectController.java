899177c56a4561ccf10a2d1f93ec7e440fa62902
inyourcare <inyourcareallen@gmail.com>
2020-03-22 11:18:38 +0900

    Initial commit

---
+++ src/main/java/com/kkh/app/controller/ProjectController.java
@@ -0,0 +1,17 @@
+//package com.kkh.app.controller;
+//
+//import io.swagger.annotations.Api;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Api(value = "project api", consumes = "application/json")
+//@RequestMapping("/v0/project")
+//public class ProjectController {
+//    @RequestMapping(value = "/test", method = RequestMethod.GET)
+//    public ResponseEntity test() {
+//        return ResponseEntity.ok().body("hello");
+//    }
+//}
